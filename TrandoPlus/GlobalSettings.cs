f6af1607bb4c4cbe74fe352bb534db146da01530
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-29 14:39:12 +0000

    Add area-door noninteraction setting, and bump version

--- TrandoPlus/GlobalSettings.cs
+++ TrandoPlus/GlobalSettings.cs
@@ -3,6 +3,7 @@
     public class GlobalSettings
     {
         public bool 门随机 = false;
+        public bool 门更易连门 = false;
         public bool 下落随机 = false;
         public bool 避免相邻椅子房间 = false;
     }
